678c0c936fc36e209de27996b5bccd8953367e6a
Sebastien <bastien.rapuzzi@gmail.com>
2024-12-11 16:11:01 +0100

    :rocket: start project

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.arima.model import ARIMA
+import pmdarima as pm
+
+
